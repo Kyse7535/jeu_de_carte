4e71263f0f982c0da7fb61b7fb34392eba810f2b
trofinn <72690795+trofinn@users.noreply.github.com>
2022-11-22 16:38:30 +0100

    Update and rename AccountPersistanceAdapter.java to PersistanceAdapter.java

---
+++ src/main/java/adapter/out/PersistanceAdapter.java
@@ -0,0 +1,23 @@
+package adapter.out;
+
+import application.port.out.Repository;
+
+public class PersistanceAdapter implements Repository {
+
+
+    @Override
+    public void save(Object o) {
+        // TO DO implementation BDD
+    }
+
+    @Override
+    public Object loadCompte(String pseudo) {
+        // TO DO implementation BDD
+        return null;
+    }
+
+    @Override
+    public void update(Object o) {
+        // TO DO implementation BDD
+    }
+}

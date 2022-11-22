ab91196659ce5fe473ee4fc01f56c50de41d7354
trofinn <72690795+trofinn@users.noreply.github.com>
2022-11-22 16:39:37 +0100

    Update CreationHerosCommand.java

--- src/main/java/application/port/in/DTOs/CreationHerosCommand.java
+++ src/main/java/application/port/in/DTOs/CreationHerosCommand.java
@@ -1,4 +1,30 @@
 package application.port.in.DTOs;
 
+import domain.Rarete;
+import domain.Specialite;
+
+import java.util.UUID;
+
 public class CreationHerosCommand {
+    private final String id;
+    private final Specialite specialite;
+    private final Rarete rarete;
+
+    public String getId() {
+        return id;
+    }
+
+    public Specialite getSpecialite() {
+        return specialite;
+    }
+
+    public Rarete getRarete() {
+        return rarete;
+    }
+
+    public CreationHerosCommand(String id, Specialite specialite, Rarete rarete) {
+        this.id = id;
+        this.specialite = specialite;
+        this.rarete = rarete;
+    }
 }

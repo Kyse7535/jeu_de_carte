5b96b1bdc1b81d9feb4c0aea1466cd2bb739fc24
trofinn <72690795+trofinn@users.noreply.github.com>
2022-11-22 16:41:33 +0100

    Create CreationHerosService.java

---
+++ src/main/java/application/services/CreationHerosService.java
@@ -0,0 +1,20 @@
+package application.services;
+
+import application.port.in.DTOs.CreationHerosCommand;
+import application.port.in.UseCases.CreationHerosUseCase;
+import application.port.out.Repository;
+import domain.Heros;
+
+public class CreationHerosService implements CreationHerosUseCase {
+    private final Repository repository;
+
+    public CreationHerosService(Repository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void create(CreationHerosCommand creationHerosCommand) {
+        Heros heros = new Heros(creationHerosCommand.getId(),creationHerosCommand.getSpecialite(),creationHerosCommand.getRarete());
+        repository.save(heros);
+    }
+}

6e907514252154995e2e7c0469ff72d45e9f79cd
trofinn <72690795+trofinn@users.noreply.github.com>
2022-11-23 16:56:56 +0100

    Update Compte.java

--- src/main/java/domain/Compte.java
+++ src/main/java/domain/Compte.java
@@ -4,10 +4,16 @@ public class Compte {
     private final String pseudo;
     private int nombre_jetons;
     private Deck deck;
+
+
     public Compte(String pseudo, Deck deck) {
         this.pseudo = pseudo;
         this.nombre_jetons = 4;
         this.deck = new Deck();
+    }
+
+    public Deck getDeck() {
+        return deck;
     }
 
 }

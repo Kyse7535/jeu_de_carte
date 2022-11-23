860ba9d8df5d07d5bf2c2632645d8186b8c25bc3
trofinn <72690795+trofinn@users.noreply.github.com>
2022-11-23 16:49:19 +0100

    Update CreationCompteCommand.java

--- src/main/java/application/port/in/DTOs/CreationCompteCommand.java
+++ src/main/java/application/port/in/DTOs/CreationCompteCommand.java
@@ -5,10 +5,12 @@ import domain.Deck;
 public class CreationCompteCommand {
     private final String pseudo;
     private Deck deck;
+    private int nbr_jetons;
 
     public CreationCompteCommand(String pseudo, Deck deck) {
         this.pseudo = pseudo;
         this.deck = deck;
+        nbr_jetons = 4;
     }
 
     public String getPseudo() {

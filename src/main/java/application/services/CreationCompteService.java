30e9d8b1b71e30fa88bd727eb15c38aa42738b6d
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-12 13:36:43 +0100

    Update CreationCompteService.java

--- src/main/java/application/services/CreationCompteService.java
+++ src/main/java/application/services/CreationCompteService.java
@@ -2,20 +2,23 @@ package application.services;
 
 import application.port.in.DTOs.CreationCompteCommand;
 import application.port.in.UseCases.CreationCompteUseCase;
-import application.port.out.Repository;
+import application.port.out.CompteRepository;
 import domain.Compte;
 
 public class CreationCompteService implements CreationCompteUseCase {
-    private final Repository<Compte> repository;
+    private final CompteRepository repository;
 
-    public CreationCompteService(Repository<Compte> repository) {
+    public CreationCompteService(CompteRepository repository) {
         this.repository = repository;
     }
 
 
     @Override
-    public void create(CreationCompteCommand creationCompteCommand) {
-        Compte compte = new Compte(creationCompteCommand.getPseudo(),creationCompteCommand.getDeck());
+    public Compte create(CreationCompteCommand creationCompteCommand) {
+        Compte compte = new Compte(creationCompteCommand.getPseudo());
+        /*if(repository.load(compte.getPseudo())==null)
+            repository.save(compte);*/
         repository.save(compte);
+        return compte;
     }
 }

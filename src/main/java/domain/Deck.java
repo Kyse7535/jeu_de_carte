1d59cd76eff988ff3ac397ccdb99c68dc67f8ac5
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-12 13:39:20 +0100

    Update Deck.java

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -4,9 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-    private List<Heros> listCartes;
+    private ArrayList<Heros> listCartes;
 
     public Deck() {
         this.listCartes = new ArrayList<>();
+    }
+
+    public void ajouteCarte(Heros heros) {
+        listCartes.add(heros);
+    }
+
+    @Override
+    public String toString() {
+        return "Deck{" +
+                "listCartes=" + listCartes +
+                '}';
     }
 }

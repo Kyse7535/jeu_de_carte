54c18c8ec2c503174e0c1ac8af473b017f33d0e5
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-12 13:34:29 +0100

    Update RechercheJoueursUseCase.java

--- src/main/java/application/port/in/UseCases/RechercheJoueursUseCase.java
+++ src/main/java/application/port/in/UseCases/RechercheJoueursUseCase.java
@@ -1,12 +1,10 @@
 package application.port.in.UseCases;
 
-import application.port.in.DTOs.RechercheHerosCommand;
 import application.port.in.DTOs.RechercheJoueursCommand;
 import domain.Compte;
-import domain.Deck;
+import java.util.ArrayList;
 
-import java.util.Map;
 
 public interface RechercheJoueursUseCase {
-    Map<Compte, Deck> recherche_liste_joueur(RechercheJoueursCommand rechercheJoueursCommand);
+    ArrayList<Compte> recherche_liste_joueur(RechercheJoueursCommand rechercheJoueursCommand);
 }

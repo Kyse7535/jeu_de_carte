5859f5791615b136200bf466fc0d2e045ed28b0e
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-12 13:31:49 +0100

    Create OuverturePackCommand.java

---
+++ src/main/java/application/port/in/DTOs/OuverturePackCommand.java
@@ -0,0 +1,18 @@
+package application.port.in.DTOs;
+
+import domain.Compte;
+import domain.Pack;
+
+public class OuverturePackCommand {
+    private final Pack pack;
+    private final Compte compte;
+
+    public OuverturePackCommand(Pack pack, Compte compte) {
+        this.pack = pack;
+        this.compte = compte;
+    }
+
+    public Pack getPack() {
+        return pack;
+    }
+}

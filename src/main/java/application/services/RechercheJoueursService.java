c26e371783510c3a18e9bff6f2ca16145ebbd929
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-12 13:37:48 +0100

    Update RechercheJoueursService.java

--- src/main/java/application/services/RechercheJoueursService.java
+++ src/main/java/application/services/RechercheJoueursService.java
@@ -1,37 +1,19 @@
 package application.services;
-
-import application.port.in.DTOs.RechercheHerosCommand;
 import application.port.in.DTOs.RechercheJoueursCommand;
 import application.port.in.UseCases.RechercheJoueursUseCase;
-import application.port.out.Repository;
+import application.port.out.CompteRepository;
 import domain.Compte;
-import domain.Deck;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class RechercheJoueursService implements RechercheJoueursUseCase {
-    private final Repository<Compte> repository;
+    private final CompteRepository repository;
 
-    public RechercheJoueursService(Repository<Compte> repository) {
+    public RechercheJoueursService(CompteRepository repository) {
         this.repository = repository;
     }
 
     @Override
-    public Map<Compte, Deck> recherche_liste_joueur(RechercheJoueursCommand rechercheJoueursCommandd) {
-
-        ArrayList<Compte> listJoueurs = repository.findAll();
-        Map<Compte, Deck> listJoueursAvecHeros = new HashMap<>();
-
-        Iterator<Compte> it = listJoueurs.iterator();
-        while(it.hasNext()) {
-            Deck listHeros = it.next().getDeck();
-            listJoueursAvecHeros.put(it.next(), listHeros);
-        }
-        return listJoueursAvecHeros;
+    public ArrayList<Compte> recherche_liste_joueur(RechercheJoueursCommand rechercheJoueursCommandd) {
+        return repository.findAllPlayers();
     }
-
-
 }

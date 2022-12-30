59f06a5352f975ead4c6858f24c17d5306ee81ab
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-30 10:10:36 +0100

    Update Pack.java

--- src/main/java/domain/Pack.java
+++ src/main/java/domain/Pack.java
@@ -3,18 +3,25 @@ package domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum Pack {
-    argent(1, 3),
-    diamant(2, 5);
+    argent(1, 3, new int[]{5, 20, 75}),
+    diamant(2, 5, new int[]{15,35,5});
 
     @JsonProperty
     public int prix;
     @JsonProperty
     public int nbr_cartes;
-    //private final Quartet<Integer, Integer, Integer> probabilites;
-    private Pack(int prix, int nbr_cartes /*Quartet<Integer, Integer, Integer> probabilites*/) {
+    private int[] probabilites = {};
+
+    private Pack(int prix, int nbr_cartes, int[] probabilites) {
         this.prix = prix;
         this.nbr_cartes = nbr_cartes;
-        //this.probabilites = probabilites;
+        this.probabilites = probabilites;
     }
+
+    public int[] getProbabilites() {
+        return probabilites;
+    }
+
     Pack() {}
+
 }

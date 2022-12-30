c3a15b14ebf87af3f725cafed66ed0bd9dd81b3a
trofinn <72690795+trofinn@users.noreply.github.com>
2022-12-30 10:08:14 +0100

    Create RechercheUnJoueurServiceTest.java

---
+++ src/test/application/services/RechercheUnJoueurServiceTest.java
@@ -0,0 +1,41 @@
+package application.services;
+
+import adapter.out.PersistanceAdapter;
+import application.port.in.DTOs.CreationCompteCommand;
+import application.port.in.DTOs.RechercheUnJoueurCommand;
+import domain.Compte;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RechercheUnJoueurServiceTest {
+
+    @InjectMocks
+    private RechercheUnJoueurService rechercheUnJoueurService;
+
+    @InjectMocks
+    private CreationCompteService creationCompteService;
+
+    @Mock
+    private PersistanceAdapter persistanceAdapter;
+
+    @Test
+    void recherche_un_joueur() {
+        Compte compte = creationCompteService.create(new CreationCompteCommand("test"));
+
+        // WHEN
+        when(persistanceAdapter.load_compte(compte.getPseudo())).thenReturn(compte);
+        Compte compte_result = rechercheUnJoueurService.recherche_un_joueur(new RechercheUnJoueurCommand(compte.getPseudo()));
+
+        // ASSERT
+        Assertions.assertEquals("test",compte_result.getPseudo());
+        Mockito.verify(persistanceAdapter).load_compte(compte.getPseudo());
+    }
+}

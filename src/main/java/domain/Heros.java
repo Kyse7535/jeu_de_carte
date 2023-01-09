305602be33a8974911ead301b3b1e488ca0ec834
trofinn <72690795+trofinn@users.noreply.github.com>
2023-01-09 14:16:15 +0100

    static new methods for creation, deleted custom constructor

--- src/main/java/domain/Heros.java
+++ src/main/java/domain/Heros.java
@@ -5,7 +5,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
+
+
 public class Heros {
+
+    private static final float POINTS_VIE_TANK = 1000;
+    private static final float POINTS_PUISSANCE_TANK = 100;
+    private static final float POINTS_ARMURE_TANK = 20;
+    private static final float POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK = 20;
+
+    private static final float POINTS_VIE_ASSASSIN = 800;
+    private static final float POINTS_PUISSANCE_ASSASSIN = 200;
+    private static final float POINTS_ARMURE_ASSASSIN= 5;
+    private static final float POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN = 30;
+
+    private static final float POINTS_VIE_MAGE = 700;
+    private static final float POINTS_PUISSANCE_MAGE = 150;
+    private static final float POINTS_ARMURE_MAGE= 10;
+    private static final float POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE= 25;
+
     @JsonProperty
     private String id;
     @JsonProperty
@@ -25,74 +43,79 @@ public class Heros {
     @JsonIgnore
     private ArrayList<Combat> combat_history;
 
+    public static Heros newCommunTank(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Tank,Rarete.Commun),POINTS_VIE_TANK,POINTS_PUISSANCE_TANK,POINTS_ARMURE_TANK,POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK);
+    }
+
+    public static Heros newRareTank(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Tank,Rarete.Rare),(POINTS_VIE_TANK+Rarete.Rare.procentage/100 * POINTS_VIE_TANK),
+                (POINTS_PUISSANCE_TANK+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_TANK),
+                (POINTS_ARMURE_TANK+Rarete.Rare.procentage/100 * POINTS_ARMURE_TANK),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK));
+    }
+
+    public static Heros newLegendaryTank(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Tank,Rarete.Legandaire),(POINTS_VIE_TANK+Rarete.Legandaire.procentage/100 * POINTS_VIE_TANK),
+                (POINTS_PUISSANCE_TANK+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_TANK),
+                (POINTS_ARMURE_TANK+Rarete.Legandaire.procentage/100 * POINTS_ARMURE_TANK),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_TANK));
+    }
+
+    public static Heros newCommunAssassin(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Assassin,Rarete.Commun),POINTS_VIE_ASSASSIN,POINTS_PUISSANCE_ASSASSIN,POINTS_ARMURE_ASSASSIN,POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN);
+    }
+
+    public static Heros newRareAssassin(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Assassin,Rarete.Rare),(POINTS_VIE_ASSASSIN+Rarete.Rare.procentage/100 * POINTS_VIE_ASSASSIN),
+                (POINTS_PUISSANCE_ASSASSIN+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_ASSASSIN),
+                (POINTS_ARMURE_ASSASSIN+Rarete.Rare.procentage/100 * POINTS_ARMURE_ASSASSIN),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN));
+    }
+
+    public static Heros newLegendaryAssassin(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Assassin,Rarete.Legandaire),(POINTS_VIE_ASSASSIN+Rarete.Legandaire.procentage/100 * POINTS_VIE_ASSASSIN),
+                (POINTS_PUISSANCE_ASSASSIN+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_ASSASSIN),
+                (POINTS_ARMURE_ASSASSIN+Rarete.Legandaire.procentage/100 * POINTS_ARMURE_ASSASSIN),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_ASSASSIN));
+    }
+
+    public static Heros newCommunMage(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Mage,Rarete.Commun),POINTS_VIE_MAGE,POINTS_PUISSANCE_MAGE,POINTS_ARMURE_MAGE,POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE);
+    }
+
+    public static Heros newRareMage(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Mage,Rarete.Rare),(POINTS_VIE_MAGE+Rarete.Rare.procentage/100 * POINTS_VIE_MAGE),
+                (POINTS_PUISSANCE_MAGE+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_MAGE),
+                (POINTS_ARMURE_MAGE+Rarete.Rare.procentage/100 * POINTS_ARMURE_MAGE),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE+Rarete.Rare.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE));
+    }
+
+    public static Heros newLegendaryMage(String id) {
+        return new Heros(id,new Caracteristiques(Specialite.Mage,Rarete.Legandaire),(POINTS_VIE_MAGE+Rarete.Legandaire.procentage/100 * POINTS_VIE_MAGE),
+                (POINTS_PUISSANCE_MAGE+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_MAGE),
+                (POINTS_ARMURE_MAGE+Rarete.Legandaire.procentage/100 * POINTS_ARMURE_MAGE),
+                (POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE+Rarete.Legandaire.procentage/100 * POINTS_PUISSANCE_SUPPLEMENTAIRE_MAGE));
+    }
+
+
+
+
+
+    public Heros(String id, Caracteristiques caracteristiques, float point_de_vie, float puissance, float armure, float puissance_supplementaire) {
+        this.id = id;
+        this.caracteristiques = caracteristiques;
+        this.puissance = puissance;
+        this.point_de_vie = point_de_vie;
+        this.armure = armure;
+        this.puissance_supplementaire = puissance_supplementaire;
+    }
+
+
     public Heros(String id, Caracteristiques caracteristiques) {
         this.id = id;
-       this.caracteristiques = caracteristiques;
-        this.niveau = 1;
-        this.combat_history = new ArrayList<>();
-        if(caracteristiques.getSpecialite().equals(Specialite.Tank)) {
-            if (caracteristiques.getRarete().equals(Rarete.Commun)) {
-                point_de_vie = 1000 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 100 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 20 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 20 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Rare)) {
-                point_de_vie = 1000 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 100 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 20 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 20 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Legandaire)) {
-                point_de_vie = 1000 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 100 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 20 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 20 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-        }
-        if(caracteristiques.getSpecialite().equals(Specialite.Assassin)) {
-            if (caracteristiques.getRarete().equals(Rarete.Commun)) {
-                point_de_vie = 800 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 200 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 5 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 30 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Rare)) {
-                point_de_vie = 800 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 200 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 5 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 30 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Legandaire)) {
-                point_de_vie = 800 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 200 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 5 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 30 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-        }
-        if(caracteristiques.getSpecialite().equals(Specialite.Mage)) {
-            if (caracteristiques.getRarete().equals(Rarete.Commun)) {
-                point_de_vie = 700 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 150 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 10 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 25 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Rare)) {
-                point_de_vie = 700 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 150 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 10 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 25 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-            if (caracteristiques.getRarete().equals(Rarete.Legandaire)) {
-                point_de_vie = 700 + (caracteristiques.getRarete().procentage/100*point_de_vie);
-                puissance = 150 + (caracteristiques.getRarete().procentage/100*puissance);
-                armure = 10 + (caracteristiques.getRarete().procentage/100*armure);
-                puissance_supplementaire = 25 + (caracteristiques.getRarete().procentage/100*armure);
-            }
-        }
-
-
+        this.caracteristiques = caracteristiques;
     }
+
 
     public Heros() {}
 

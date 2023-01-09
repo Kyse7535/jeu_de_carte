9ffd79407d26f3be8b274d10c2699c2f9e8f2e1a
trofinn <72690795+trofinn@users.noreply.github.com>
2023-01-09 14:57:01 +0100

    Adding domain rules

--- src/main/java/application/services/CombatService.java
+++ src/main/java/application/services/CombatService.java
@@ -15,10 +15,20 @@ public class CombatService implements CombatUseCase {
     @Override
     public Combat attack(CombatCommand combatCommand) {
         Combat combat = combatCommand.getCombat();
-        combat.attaque();
-        //combat.combat_history();
-        repository.update(combatCommand.getCombat().getAttaquant());
-        repository.update(combatCommand.getCombat().getAdversaire());
-        return combat;
+        if (combat.getAdversaire().getNiveau() < combat.getAttaquant().getNiveau()) {
+            throw new RuntimeException("le heros a le niveau trop petit");
+        }
+        else if (combat.getAdversaire().getEn_vie() == false || combat.getAttaquant().getEn_vie() == false) {
+            throw new RuntimeException("un heros mort ne peut pas participer a un combat.");
+        }
+        else
+        {
+            combat.attaque();
+            //combat.combat_history();
+            repository.update(combatCommand.getCombat().getAttaquant());
+            repository.update(combatCommand.getCombat().getAdversaire());
+            return combat;
+        }
+
     }
 }

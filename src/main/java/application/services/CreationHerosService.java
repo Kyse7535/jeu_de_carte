f5b817ebf7f829780c7240e3b20daf1d8ea149c7
trofinn <72690795+trofinn@users.noreply.github.com>
2023-01-09 14:17:52 +0100

    adapt service to new static creation hero methods

--- src/main/java/application/services/CreationHerosService.java
+++ src/main/java/application/services/CreationHerosService.java
@@ -4,6 +4,8 @@ import application.port.in.DTOs.CreationHerosCommand;
 import application.port.in.UseCases.CreationHerosUseCase;
 import application.port.out.HerosRepository;
 import domain.Heros;
+import domain.Rarete;
+import domain.Specialite;
 
 public class CreationHerosService implements CreationHerosUseCase {
     private final HerosRepository repository;
@@ -14,7 +16,41 @@ public class CreationHerosService implements CreationHerosUseCase {
 
     @Override
     public Heros create(CreationHerosCommand creationHerosCommand) {
-        Heros heros = new Heros(creationHerosCommand.getId(),creationHerosCommand.getCaracteristiques());
+        Heros heros = new Heros();
+        if(creationHerosCommand.getCaracteristiques().getSpecialite().equals(Specialite.Tank)) {
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Commun)) {
+                heros = Heros.newCommunTank(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Rare)) {
+                heros = Heros.newRareTank(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Legandaire)) {
+                heros = Heros.newLegendaryTank(creationHerosCommand.getId());
+            }
+        }
+        if(creationHerosCommand.getCaracteristiques().getSpecialite().equals(Specialite.Assassin)) {
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Commun)) {
+                heros = Heros.newCommunAssassin(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Rare)) {
+                heros = Heros.newRareAssassin(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Legandaire)) {
+                heros = Heros.newLegendaryAssassin(creationHerosCommand.getId());
+            }
+        }
+        if(creationHerosCommand.getCaracteristiques().getSpecialite().equals(Specialite.Mage)) {
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Commun)) {
+                heros = Heros.newCommunMage(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Rare)) {
+                heros = Heros.newRareMage(creationHerosCommand.getId());
+            }
+            if(creationHerosCommand.getCaracteristiques().getRarete().equals(Rarete.Legandaire)) {
+                heros = Heros.newLegendaryMage(creationHerosCommand.getId());
+            }
+        }
+
         repository.save(heros);
         return heros;
     }
